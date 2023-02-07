71d4cf221261a07d180438575955f3aa82642c54
ttak0422 <ttak0422@gmail.com>
2023-02-08 01:24:38 +0900

    Refactor: optimize performance

--- lua/winbar/init.lua
+++ lua/winbar/init.lua
@@ -6,9 +6,15 @@ function M.setup(opts)
     config.set_options(opts)
     local winbar = require('winbar.winbar')
 
+    local events = { 'DirChanged', 'BufWinEnter', 'BufFilePost', 'BufWritePost' }
+    if opts.show_symbols then
+      table.insert(events, 'CursorMoved')
+      table.insert(events, 'InsertEnter')
+    end
+
     winbar.init()
     if opts.enabled == true then
-        vim.api.nvim_create_autocmd({ 'DirChanged', 'CursorMoved', 'BufWinEnter', 'BufFilePost', 'InsertEnter', 'BufWritePost' }, {
+        vim.api.nvim_create_autocmd(events, {
             callback = function()
                 winbar.show_winbar()
             end
